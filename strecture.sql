1e6fc402d58c6cfae9b5f39a6cf43ba75a571e7f
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-04-15 10:38:48 +0100

    creat table users

--- strecture.sql
+++ strecture.sql
@@ -4,3 +4,13 @@ CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     label VARCHAR(225)
 );
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(225),
+    lastName VARCHAR(225),
+    emil VARCHAR(225),
+    password VARCHAR(225),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
